41851467cda44e7ba5205a53340d397fb0ef51b5
cristian0223 <cc7847443@gmail.com>
2023-09-01 00:17:22 -0500

    Avance ProyectoInvestigacion

--- Investigacion.Shared/Entities/ProyectoInvestigacion.cs
+++ Investigacion.Shared/Entities/ProyectoInvestigacion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,28 @@ namespace Investigacion.Shared.Entities
 {
     internal class ProyectoInvestigacion
     {
+        [Required(ErrorMessage ="El nombre es obligatorio")]
+        public string Nombre {  get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La fecha es obligatoria")]
+
+        public DateTime FechaInicio { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+
+        public string Fidelizacion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+
+        public string LiderProyecto {  get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+
+        public string Descripcion {  get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+
+        public string AreaInvestigacion {  get; set; } = string.Empty;
+
     }
 }
